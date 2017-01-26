9cd7290cecdfc8039c548cb9d509957797ac6596
sudarshanchoubey <sudarshan.choubey@gmail.com>
2017-01-26 23:24:11 +0530

    Changed controller

--- src/fileguard/FXMLDocumentController.java
+++ src/fileguard/FXMLDocumentController.java
@@ -63,7 +63,7 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        choiceBox.se
     }    
     
 }
